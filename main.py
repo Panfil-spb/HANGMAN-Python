96698fc121db3104b9bb0600728e9d3eaa622def
Andrey <panfilovich1@gmail.com>
2020-12-18 01:54:44 +0300

    adding a welcome function

---
+++ main.py
@@ -0,0 +1,20 @@
+import random
+
+def hello():
+    print("""
+                        Привет, это игра Висилица.
+            Суть игры - это отгадывать слово по буква, количевство попыток ограничено.
+              С каждым неверным выбором буквы, появляется новый рисунок повешенного.
+                                Приятной игры!
+    ______________________________________________________________________________________________
+                        Hello, this is Hangman game.
+        The essence of the game is to guess a word letter by letter, the number of attempts is limited.
+                With each wrong letter choice, a new drawing of the hanged man appears.
+                            Have a nice game!
+            """)
+
+
+def main():
+    hello()
+
+main()
